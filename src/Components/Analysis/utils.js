3d1b214646beb58ac669b10450578a8347692be2
SHIBIN K A <shibin.k.a6235@gmail.com>
2025-01-24 19:36:53 +0530

    feat: analysis page design completed

--- src/Components/Analysis/utils.js
+++ src/Components/Analysis/utils.js
@@ -9,3 +9,36 @@ export const getColor = ({theme})=>{
           // code block
       }
 }
+
+export const data = [
+  { day: '2023-01-01', value: 10 },
+  { day: '2023-01-02', value: 30 },
+  { day: '2023-01-03', value: 20 },
+  // Add more data here
+];
+/*
+<ResponsiveCalendar
+              data={data}
+              width={600}
+              from="2023-01-01"
+              to="2023-12-31"
+              emptyColor="#595959"
+              colors={[ '#61cdbb', '#97e3d5', '#e8c1a0', '#f47560' ]}
+              margin={{ top: 0, right: 0, bottom: 40, left: 40 }}
+              yearSpacing={40}
+              monthBorderColor="#000000"
+              dayBorderWidth={2}
+              dayBorderColor="#000000"
+              legends={[
+                  {
+                      anchor: 'bottom-right',
+                      direction: 'row',
+                      translateY: 36,
+                      itemCount: 4,
+                      itemWidth: 36,
+                      itemHeight: 36,
+                      itemsSpacing: 14,
+                      itemDirection: 'right-to-left'
+                  }
+              ]}
+        />*/
--- src/Components/AtomicDesign/Atom/Button/utils.js
+++ src/Components/AtomicDesign/Atom/Button/utils.js
@@ -1,18 +1,18 @@
 import classNames from "classnames"
 
 export const getBtnClasses = ({ variant, variantType, className, isLoading }) => {
-    const btnClasses = classNames("py-2 px-2 rounded flex items-center justify-center text-white font-bold", {
-        " bg-primary cursor-pointer":
+    const btnClasses = classNames("py-2 px-2 rounded flex items-center justify-center font-bold", {
+        " bg-primary cursor-pointer text-white":
             variant === "primary" && variantType === "solid",
-        " bg-secondary cursor-pointer":
+        " bg-secondary cursor-pointer text-white":
             variant === "secondary" && variantType === "solid",
-        " bg-tertiary cursor-pointer":
+        " bg-tertiary cursor-pointer text-white":
             variant === "tertiary" && variantType === "solid",
-        " border-2 border-primary hover:bg-primary cursor-pointer":
+        " border border-primary hover:bg-primary cursor-pointer text-primary hover:text-white":
             variant === "primary" && variantType === "outline",
-        " border-2 border-secondary hover:bg-secondary cursor-pointer":
+        " border-2 border-secondary hover:bg-secondary cursor-pointer text-secondary":
             variant === "secondary" && variantType === "outline",
-        " border-2 border-tertiary hover:bg-tertiary cursor-pointer":
+        " border-4 border-tertiary hover:bg-tertiary cursor-pointer text-tertiary":
             variant === "tertiary" && variantType === "outline",
         " cursor-no-drop opacity-70": isLoading
     }, className);
